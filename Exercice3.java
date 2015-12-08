977a41fbaedb93eacde94218db1615e68e8d70c1
Karin Touchie <ktouchie@gmail.com>
2015-12-08 17:13:40 +0100

    Change variables and operations

--- Exercice3.java
+++ Exercice3.java
@@ -3,12 +3,19 @@ class Exercice1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the base of the triangle:");
-        int base = sc.nextInt();
-        System.out.println("Enter the height of the triangle:");
-        int height = sc.nextInt();
-        int area = ((base*height)/2);
-        System.out.println("The area of the triangle is " + area);
+        System.out.println("Enter x:");
+        int x = sc.nextInt();
+        System.out.println("Enter y:");
+        int y = sc.nextInt();
+        System.out.println("Before...");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        int a = x;
+        x = y;
+        y = a;
+        System.out.println("After...");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
 
 	}
 
